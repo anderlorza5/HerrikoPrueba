972ad951d6002a551a9ffe3bb0a43ea38fec1ac9
Ander Lorza <anderlorza210194@gmail.com>
2023-04-20 23:07:47 +0200

    implementado firebase

---
+++ app/src/main/java/com/example/herrikoprueba/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.herrikoprueba;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
